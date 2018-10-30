593d7e895e960c6e6fb529030c31faaacb5a3450
techexp37 <washawasha630@gmail.com>
2018-10-30 13:59:40 +0900

    Change-user_model

---
+++ db/migrate/20181028082620_remove_address_id_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveAddressIdFromUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :address_id, :integer
+  end
+end
---
+++ db/migrate/20181028092830_change_column_to_user.rb
@@ -0,0 +1,10 @@
+class ChangeColumnToUser < ActiveRecord::Migration[5.1]
+  def change
+      change_column :users, :family_name, :string, null: true
+      change_column :users, :first_name, :string, null: true
+      change_column :users, :family_name_kana, :string, null: true
+      change_column :users, :first_name_kana, :string, null: true
+      change_column :users, :introduction, :text, null: true
+      change_column :users, :birthday, :date, null: true
+  end
+end
