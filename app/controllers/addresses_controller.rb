638f3d730adcc00a7e9da875a633c71dded9b925
techexp37 <washawasha630@gmail.com>
2018-11-03 00:02:03 +0900

    Edit-address_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,7 @@
 class AddressesController < ApplicationController
   def new
     if user_signed_in?
-      @user = User.find_by(id: params[:user_id])
+      @user = User.find(params[:user_id])
       @address = Address.new
     else
       redirect_to users_path
@@ -9,9 +9,9 @@ class AddressesController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: current_user.id)
-    @address = Address.new(family_name: address_params[:family_name], first_name: address_params[:first_name], family_name_kana: address_params[:family_name_kana], first_name_kana: address_params[:first_name_kana], postal_code: address_params[:postal_code], prefecture: address_params[:prefecture], city: address_params[:city], street_number: address_params[:street_number], building:
-      address_params[:building], telephone: address_params[:telephone], user_id: current_user.id)
+    @user = User.find(current_user.id)
+    @address = Address.new(address_params)
+    @address.user_id = current_user.id
     if @address.save
       redirect_to root_path
     else
