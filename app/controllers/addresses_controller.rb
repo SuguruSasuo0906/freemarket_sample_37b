23a719f4dbf7844c24635cb2d6b66d059bd5947f
techexp37 <washawasha630@gmail.com>
2018-11-03 15:11:51 +0900

    Modify-address_controller&_test_code

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,8 @@
 class AddressesController < ApplicationController
+  before_action :set_user
+
   def new
     if user_signed_in?
-      @user = User.find(params[:user_id])
       @address = Address.new
     else
       redirect_to users_path
@@ -9,9 +10,7 @@ class AddressesController < ApplicationController
   end
 
   def create
-    @user = User.find(current_user.id)
     @address = Address.new(address_params)
-    @address.user_id = current_user.id
     if @address.save
       redirect_to root_path
     else
@@ -20,7 +19,12 @@ class AddressesController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
   def address_params
-    params.require(:address).permit(:family_name,:first_name,:family_name_kana,:first_name_kana,:postal_code,:prefecture,:city,:street_number, :building, :telephone)
+    params.require(:address).permit(:family_name,:first_name,:family_name_kana,:first_name_kana,:postal_code,:prefecture,:city,:street_number, :building, :telephone).merge(user_id: current_user.id)
   end
 end
