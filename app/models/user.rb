72f97b8bda9668dfa13cddca564a1f62c7ad5cf2
techexp37 <washawasha630@gmail.com>
2018-11-06 20:59:29 +0900

    Add-facebook_confirm_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_one :address, dependent: :destroy
   has_one :payment, dependent: :destroy
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        nickname: auth.info.name,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 
   validates :nickname,
     presence: true,
