0846394b7f824174e631482e6fdd544005fbd30a
techexp37 <washawasha630@gmail.com>
2018-11-07 12:17:14 +0900

    Add-google_oauth_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,17 +3,30 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :facebook
   end
 
+   def google_oauth2
+    callback_from_google
+   end
+
   private
 
   def callback_from(provider)
     provider = provider.to_s
-
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_path
+    end
+  end
+
+  def callback_from_google
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_path
     end
   end
